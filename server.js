d37039f2f140095134053b64c484125ea226d954
David Cohen <dcohen@infinio.com>
2017-02-06 16:21:38 -0500

    Doing some not particularly helpful other than as an example request logging

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 router.use(function (req, res, next) {
     // do logging
-    console.log('An API request has been made');
+    console.log('An API request has been made: ' + req.rawHeaders);
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -25,9 +25,9 @@ router.get('/', function (req, res) {
     res.json({message: 'Nothing here at the default route'});
 });
 
+app.use('/api', router);
 app.use('/api/pongs', require('./app/controllers/pongController'))
 app.use('/api/events', require('./app/controllers/eventController'))
-app.use('/api', router);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
